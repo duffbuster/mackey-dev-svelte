d21e9c5c8acfbbbdb42c2d9bf99a13e10af352b0
Colin Mackey <colin.mackey@penn-interactive.com>
2022-07-26 13:22:56 -0400

    added hero section

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,21 @@
+<div class="hero">
+  <div class="hero-text">
+    <h1 class="display-76 white">Hi, I'm Colin.</h1>
+    <p class="white">Philadelphia, PA</p>
+  </div>
+</div>
+
+<style>
+  .hero {
+    background-image: url('/pexels-kelly-4642388.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding: 300px 0;
+  }
+
+  .hero-text {
+    margin: auto;
+    max-width: 60vw;
+  }
+</style>
