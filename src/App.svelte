6cea0c4a4f9f225c4d80071d25a3cf0790f3e206
Colin Mackey <colin.mackey@penn-interactive.com>
2023-04-19 18:14:51 -0400

    adds google analytics

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,23 @@
   import Testimonials from './lib/Testimonials.svelte';
 </script>
 
+<svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XR0DQDF850"></script>
+  <script>
+    if (typeof window !== 'undefined' && window) {
+      const gaId = 'G-XR0DQDF850';
+
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', gaId);
+    }
+  </script>
+</svelte:head>
+
 <div class="mackeydev animate__animated animate__fadeIn">
   <Header />
   <Hero />
@@ -15,6 +32,3 @@
   <Testimonials />
   <Footer />
 </div>
-
-<style>
-</style>
